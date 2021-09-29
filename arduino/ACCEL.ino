ef58279233b3ef5cae0395fe3ee1ff1b33475d8a
Controllers-ENSI <91493709+Controllers-ENSI@users.noreply.github.com>
2021-09-29 23:19:52 +0100

    Final

--- arduino/ACCEL.ino
+++ arduino/ACCEL.ino
@@ -1,41 +1,44 @@
 #include <ArduinoBLE.h>
 #include "Arduino_BHY2.h"
 #include "Nicla_System.h"
-const int UPDATE_FREQUENCY = 5;
-long previousMillis = 0;
-using namespace nicla;
-SensorXYZ accelerometer(31);
-Sensor hum(131);
-BLEService tempService("181A"); // create service
-// create characteristic and allow remote device to read and write
-BLEIntCharacteristic tempCharacteristic("2A6E",  BLERead | BLENotify);
-// create characteristic and allow remote device to get notifications and read the value
 
+using namespace nicla;
+
+const int UPDATE_FREQUENCY = 5;     // Update frequency in ms
+long previousMillis = 0;
+SensorXYZ accel(31);
+
+BLEService accelerationService("181A"); // Standard Acceleration Sensing service
+
+// create characteristics
+BLEIntCharacteristic accelCharacteristic("2A6E",               // Standard 16-bit Acceleration characteristic
+                                        BLERead | BLENotify); // Remote clients can read and get updates
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
   // begin initialization
- 
+  Serial.begin(115200);
   BHY2.begin();
   nicla::begin();
-   
-   accelerometer.configure(200, 0);
-
+  
+  accel.configure(200, 0);
 
   // set the local name peripheral advertises
   BLE.setLocalName("ACCEL");
+  
   // set the UUID for the service this peripheral advertises:
-  BLE.setAdvertisedService(tempService);
+  BLE.setAdvertisedService(accelerationService); // Advertise acceleration service
 
   // add the characteristics to the service
-  tempService.addCharacteristic(tempCharacteristic);
-
+  accelerationService.addCharacteristic(accelCharacteristic);     // Add acceleration characteristic
+ 
   // add the service
-  BLE.addService(tempService);
+  BLE.addService(accelerationService);
 
-  tempCharacteristic.writeValue(0);
+  accelCharacteristic.setValue(0);     // Set initial acceleration value
+  
   // start advertising
   BLE.advertise();
 
@@ -44,26 +47,25 @@ void setup() {
 
 void loop() {
 
-  BLEDevice central = BLE.central();  // Wait for a BLE central to connect
+  unsigned long acceleration_reading;
 
+  BLEDevice central = BLE.central();  // Wait for a BLE central to connect
+   
   // If central is connected to peripheral
   if (central) {
     Serial.println("Central connected");
-
+     BHY2.update();
     while (central.connected()) {
-       BHY2.update();
+     
       long currentMillis = millis();
-      // Check temperature & humidity with UPDATE_FREQUENCY
+            
       if (currentMillis - previousMillis >= UPDATE_FREQUENCY) {
-        previousMillis = currentMillis;
-       
         
-        Serial.println(accelerometer.z());
-    
-        tempCharacteristic.writeValue(accelerometer.z());
-      }
+                previousMillis = currentMillis;
+                Serial.println(accel.z());
+                accelCharacteristic.writeValue(accel.z());
+            }
     }
     Serial.println("Central disconnected");
   }
-
 }
